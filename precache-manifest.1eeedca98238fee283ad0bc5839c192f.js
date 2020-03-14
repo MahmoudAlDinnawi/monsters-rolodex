e1a1999be6d2dc55c68726bded2c01216d6ffd8e
mahmoud al dinnawi <mahmoudaldinnawi@mahmouds-MacBook-Air.local>
2020-03-15 00:10:51 +0200

    Updates

---
+++ precache-manifest.1eeedca98238fee283ad0bc5839c192f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "771bdb904d54afd5c82c284fffe75789",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "8086772809ad3d757665",
+    "url": "/monsters-rolodex/static/css/main.e02863f2.chunk.css"
+  },
+  {
+    "revision": "640fa3f2e5b37ee859bf",
+    "url": "/monsters-rolodex/static/js/2.c2a893ac.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-rolodex/static/js/2.c2a893ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8086772809ad3d757665",
+    "url": "/monsters-rolodex/static/js/main.a588c850.chunk.js"
+  },
+  {
+    "revision": "420e781ce2f2f4d61faa",
+    "url": "/monsters-rolodex/static/js/runtime-main.9cf5dfda.js"
+  }
+]);
